37ea2c6459ffd43f0cc2400865ddd990265b35ff
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 17:39:55 -0500

    Add and extract Stories and PostHeader views

--- Instagram_Home/ContentView.swift
+++ Instagram_Home/ContentView.swift
@@ -12,7 +12,11 @@ struct ContentView: View {
         VStack(spacing: 0) {
             Header()
             
-            Story()
+            Stories()
+            
+            Divider()
+            
+            PostHeader()
             
             Spacer()
         }
@@ -44,10 +48,13 @@ struct Header: View {
 }
 
 struct Story: View {
+    var userImage: String = "profile"
+    var userName: String = "Willie Yam"
+    
     var body: some View {
         VStack {
             VStack {
-                Image("profile")
+                Image(userImage)
                     .resizable()
                     .frame(width: 60, height: 60)
                     .cornerRadius(50)
@@ -57,9 +64,52 @@ struct Story: View {
                     .stroke(LinearGradient(colors: [.red, .purple, .red, .orange, .yellow, .orange], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2.3)
                     .frame(width: 68, height: 68)
             )
+            .frame(width: 70, height: 70)
             
-            Text("Willie Yam")
+            Text(userName)
                 .font(.caption)
         }
+    }
+}
+
+struct Stories: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 15) {
+                Story()
+                Story(userImage: "profile2", userName: "Meng To")
+                Story(userImage: "profile3", userName: "Akson")
+                Story(userImage: "profile4", userName: "Steph")
+                Story(userImage: "profile5", userName: "Sam")
+                Story(userImage: "profile6", userName: "Dara")
+                Story(userImage: "profile7", userName: "Sourany")
+                Story(userImage: "profile8", userName: "Pom")
+            }
+            .padding(.horizontal, 8)
+        }
+        .padding(.vertical, 10)
+    }
+}
+
+struct PostHeader: View {
+    var body: some View {
+        HStack {
+            HStack {
+                Image("profile")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .cornerRadius(50)
+                
+                Text("Willie Yam")
+                    .font(.caption)
+                    .fontWeight(.bold)
+            }
+            
+            Spacer()
+            
+            Image("more")
+        }
+        .padding(.vertical, 10)
+        .padding(.horizontal, 8)
     }
 }
