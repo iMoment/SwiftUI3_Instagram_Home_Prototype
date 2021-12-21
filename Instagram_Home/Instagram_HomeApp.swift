f04e066b60d757b9504b3c28cc49886e3454a517
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 16:50:32 -0500

    Initial Commit

---
+++ Instagram_Home/Instagram_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_HomeApp.swift
+//  Instagram_Home
+//
+//  Created by Stanley Pan on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
